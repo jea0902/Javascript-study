cf6eeafa5e13d819d3553085a476038888af8cde
Jaehyuck_Choi <jea0902@naver.com>
2023-11-17 16:53:24 +0900

    2023-11-17

---
+++ test1-number.js
@@ -0,0 +1,34 @@
+let arr = [1,2,3,4,5];
+
+// 2023 - 11 - 17 자바스크립트 개념 첫 공부 날
+
+// 컨트롤 + 알트 + N하면 실행됨.
+// <- 익스텐션에서 Code Runner 다운받아서 가능함.
+// 앞으로 자바스크립트 코드 공부는 js파일 만들어가면서 git push
+
+// VScode에서 공부하는 거 은근 불편하네; console.log 필순데?
+
+console.log("log:",arr);
+
+// -------------
+
+// 숫자
+
+console.log(6/2);
+
+Math.pow(3,2); // 3의 제곱
+console.log(Math.pow(3,2));
+
+console.log(Math.round(10.6)); // 반올림
+
+console.log(Math.ceil(10.2)); // 그냥 소수점 올림
+
+console.log(Math.floor(10.2)); // 그냥 소수점 내림
+
+console.log(Math.sqrt(9)); // 루트
+
+console.log(Math.random()); // 1보다 작은 랜덤 수
+
+console.log(100*Math.random()); // 100곱하기 랜덤 수
+
+console.log(Math.round(100*Math.random())); // 뒤부터 계산하고 그거를 반올림
---
+++ test1-string.js
@@ -0,0 +1,19 @@
+console.log("문자열");
+
+console.log('ㅎㅇ');
+
+console.log("'ㅎㅇ'");
+
+console.log('"ㅎㅇ"');
+
+console.log("어퍼스트로피's");
+
+console.log('어퍼스트로피"s');
+
+console.log('작은 따옴표 안에 작은따옴표를 한개만 넣으려면\'역슬래시가 필요해 역슬래시는 역슬래시 바로 다음 문자 하나는 정보로쎃 해석하자는 약속이 되어 있음.');
+// 역슬래시 바로 다음 문자는 그냥 문자로써 해석하자는 약속
+// escape문임 :  원래 가지고 있던 임무로부터 도망시킨다는 뜻
+
+console.log(typeof 1);
+console.log(typeof "1");
+
